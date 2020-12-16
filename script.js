62939e98b01fd10628f35f57decb4951d23d6961
junlaiburger <56894688+junlaiburger@users.noreply.github.com>
2020-12-15 16:04:05 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function computerPlay() {
+  playRound;
+}
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  // randomly returns either 'Rock', 'Paper' or 'Scissors'
+  let hand = 
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  playRound;
+}
